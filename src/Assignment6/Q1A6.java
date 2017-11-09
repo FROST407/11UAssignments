7365bdee0fc3e8a5168c36998e8b9842892151de
Masoa4207 <Masoa4207@HRH-IC0027345.wrdsb.ca>
2017-11-09 13:29:09 -0500

    A4Q1-5

---
+++ src/Assignment6/Q1A6.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Masoa4207
+ */
+public class Q1A6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] expenses = new double[6];
+        String[] expenseNames = new String[6];
+        expenseNames[0] = "Food";
+        expenseNames[1] = "DJ";
+        expenseNames[2] = "Hall Rental";
+        expenseNames[3] = "Decorations";
+        expenseNames[4] = "Staff Fees";
+        expenseNames[5] = "Miscellaneous";
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("How much does the " + expenseNames[i] + " cost?");
+            expenses[i] = input.nextDouble();
+        }
+        double total = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            total = total + expenses[i];
+        }
+        int tickets = (int) Math.ceil(total / 35);
+        System.out.println("Prom will cost " + total);
+        System.out.println("You will need to sell " + tickets + " tickets");
+    }
+}
